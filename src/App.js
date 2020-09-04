1a561d61998953c61a94e8d2297fcba38e599c50
woxjro <n0i6c0k2@gmail.com>
2020-09-05 08:17:35 +0900

    linksを配列で処理

--- src/App.js
+++ src/App.js
@@ -11,38 +11,52 @@ import Copyright from "./components/copyright";
 
 library.add(fab, fas, far); //他のコンポーネントから簡単に呼び出せるようにするための登録処理？
 function App() {
+   const LinkInfoArr = [
+      {
+         size: "lg",
+         color: "white",
+         icon: ["fab", "github"],
+         linkUrl: "https://github.com/woxjro/",
+         linkName: "@woxjro",
+      },
+      {
+         size: "lg",
+         color: "white",
+         icon: ["fab", "twitter"],
+         linkUrl: "https://twitter.com/woxjro",
+         linkName: "@woxjro",
+      },
+      {
+         size: "lg",
+         color: "white",
+         icon: ["fas", "university"],
+         linkUrl: "",
+         linkName: "Kyoto Univ.",
+      },
+      {
+         size: "lg",
+         color: "white",
+         icon: ["fas", "building"],
+         linkUrl: "http://x-point-1.net/",
+         linkName: "X.1",
+      },
+   ];
    return (
       <div className="App">
          <SwitchIcon />
          <div className="links">
-            <Link
-               size="lg"
-               color="white"
-               icon={["fab", "github"]}
-               linkUrl={"https://github.com/woxjro/"}
-               linkName={"@woxjro"}
-            />
-            <Link
-               size="lg"
-               color="white"
-               icon={["fab", "twitter"]}
-               linkUrl={"https://twitter.com/woxjro"}
-               linkName={"@woxjro"}
-            />
-            <Link
-               size="lg"
-               color="white"
-               icon={["fas", "university"]}
-               linkUrl={""}
-               linkName={"Kyoto Univ."}
-            />
-            <Link
-               size="lg"
-               color="white"
-               icon={["fas", "building"]}
-               linkUrl={"http://x-point-1.net/"}
-               linkName={"X.1"}
-            />
+            {LinkInfoArr.map((LinkInfo) => {
+               const { size, color, icon, linkUrl, linkName } = LinkInfo;
+               return (
+                  <Link
+                     size={size}
+                     color={color}
+                     icon={icon}
+                     linkUrl={linkUrl}
+                     linkName={linkName}
+                  />
+               );
+            })}
          </div>
          <div className="copyright-box">
             <Copyright />
